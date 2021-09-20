d3aee2116f1e902a65b4c067a90d461c9ad92ee3
Ahmed Faraz <ahmedfaraz.business@gmail.com>
2021-09-20 20:38:44 +0500

    Update About.tsx

--- src/Components/pages/About.tsx
+++ src/Components/pages/About.tsx
@@ -7,6 +7,12 @@ const About:React.FC = () => {
             <p>Developed by <span><a href="https://github.com/aahmedfaraz" target="_blank" rel="noreferrer">Ahmed Faraz</a></span></p>
             <p>Developed using <span>React</span> and <span>Typescript</span></p>
             <p><span className="version">Version: 2.0.0</span></p>
+            <h2>New Features</h2>
+            <ul>
+                <li><i style={{color: 'limegreen', margin: '0 5px 0 0'}} className="fas fa-check"></i>Offline Availability</li>
+                <li><i style={{color: 'limegreen', margin: '0 5px 0 0'}} className="fas fa-check"></i>Installable</li>
+                <li><i style={{color: 'limegreen', margin: '0 5px 0 0'}} className="fas fa-check"></i>Push Notifications</li>
+            </ul>
             <h2>Tech Used</h2>
             <ul>
                 <li>React</li>
@@ -22,7 +28,7 @@ const About:React.FC = () => {
                 <li>Netlify Hosting</li>
                 <li>Icons using Adobe Illustrator</li>
             </ul>
-            <p>Now <span>Installable</span> and You will recieve <span>daily Push Notifications</span></p>
+            <p>Now <span>Installable</span> and You will recieve <span>Daily Push Notifications</span></p>
             <small><strong>Note:</strong> Push Notifications are recieved if you will be having a stable internet connection, the app should be Installed and your notifications for the app and Google should be allowed, then in InActive(Closed) state you will recieve the notification according to scheduled time</small>
         </div>
     )
